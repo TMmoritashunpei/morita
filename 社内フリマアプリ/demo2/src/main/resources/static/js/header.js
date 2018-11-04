5a2ccef0f2582d7c4df7455251c5269b544e1b88
shunpei <shunpei5555@i.softbank.jp>
2018-11-05 00:58:48 +0900

    更新

---
+++ 社内フリマアプリ/demo2/src/main/resources/static/js/header.js
@@ -0,0 +1,11 @@
+$(function() {
+	$('#contact-show').click(function() {
+		$('#contact-modal').fadeIn();
+	});
+	$('#login-show').click(function() {
+		$('#login-modal').fadeIn();
+	});
+	$('.signup-show').click(function() {
+		$('#signup-modal').fadeIn();
+	});
+});
