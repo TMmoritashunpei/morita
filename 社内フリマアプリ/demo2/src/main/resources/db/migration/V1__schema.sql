bff951271dc9ad7a6328452a2148849c19cd40a9
morita shunpei <shunpei5555@i.softbank.jp>
2019-03-01 09:58:39 +0900

    更新

--- 社内フリマアプリ/demo2/src/main/resources/db/migration/V1__schema.sql
+++ 社内フリマアプリ/demo2/src/main/resources/db/migration/V1__schema.sql
@@ -23,6 +23,14 @@ CREATE TABLE IF NOT EXISTS categorys (
   	updated_at timestamp not null default current_timestamp on update current_timestamp
 ) character set 'utf8';
 
+CREATE TABLE IF NOT EXISTS purchases (
+	id INT(10) PRIMARY KEY AUTO_INCREMENT,
+	purchase_name VARCHAR(20),
+	created_at timestamp not null default current_timestamp,
+  	updated_at timestamp not null default current_timestamp on update current_timestamp
+) character set 'utf8';
+
+
 /*h2
 CREATE TABLE IF NOT EXISTS items (
 	id INT PRIMARY KEY AUTO_INCREMENT, 
@@ -47,5 +55,12 @@ CREATE TABLE IF NOT EXISTS categorys (
 	category_name VARCHAR(20),
 	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS purchases (
+	id INT(10) PRIMARY KEY AUTO_INCREMENT,
+	purchase_name VARCHAR(20),
+	created_at timestamp not null default current_timestamp,
+  	updated_at timestamp not null default current_timestamp on update current_timestamp
 );
 */
