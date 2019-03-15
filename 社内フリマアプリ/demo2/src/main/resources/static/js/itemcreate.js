a03454ac61892a3818f9f510799a6773c4b9a80f
morita shunpei <shunpei5555@i.softbank.jp>
2019-03-15 19:49:01 +0900

    更新

--- 社内フリマアプリ/demo2/src/main/resources/static/js/itemcreate.js
+++ 社内フリマアプリ/demo2/src/main/resources/static/js/itemcreate.js
@@ -66,4 +66,32 @@ $(function() {
 	  $('#file2').val("");
 	  $('.pic-control').append('<img src="/image/No_Image_Available.jpg">')
   })
+  var _return_value = "";
+  
+//入力値の半角数字チェック
+function check_numtype(obj){
+
+   // ２．変数の定義
+   var txt_obj = $(obj).val();
+   var text_length = txt_obj.length;
+
+   // ３．入力した文字が半角数字かどうかチェック
+   if(txt_obj.match(/^[0-9]+$/)){
+       // ３．１．文字数チェック
+       if(text_length > 9){
+           $('input[name="price"]').val(_return_value);
+       }else{
+           _return_value = txt_obj;
+       }
+   }else{
+       // ３．２．入力した文字が半角数字ではないとき
+       if(text_length == 0){
+           $('input[name="price"]').val("");
+           _return_value = "";
+       }else{
+           $('input[name="price"]').val(_return_value);
+       }
+   }
+}
+  
 });
